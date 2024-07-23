76724d7f867aaca0b35a62062471a5a4ade0d1e3
Ivan Klymenchenko <hello@iillexial.me>
2024-07-23 13:25:40 +0200

    Change total rows formatting

--- pkg/csvcopy/csvcopy.go
+++ pkg/csvcopy/csvcopy.go
@@ -301,7 +301,7 @@ func (c *Copier) report(ctx context.Context) {
 			totalTook := now.Sub(start)
 
 			c.logger.Infof(
-				"at %v, row rate %0.2f/sec (period), row rate %0.2f/sec (overall), %2.f total rows",
+				"at %v, row rate %0.2f/sec (period), row rate %0.2f/sec (overall), %0.2f total rows",
 				totalTook-(totalTook%time.Second),
 				rowrate,
 				overallRowrate,
