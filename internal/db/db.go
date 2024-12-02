ea1f05fe075e5598ec9e3632b4c9e49fc3da5d9c
Victor Perez <vperez@timescale.com>
2024-12-02 17:39:14 +0100

    cleanup

--- internal/db/db.go
+++ internal/db/db.go
@@ -4,67 +4,10 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"os"
-	"regexp"
 
 	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/jmoiron/sqlx"
 )
-
-// Overrideable is an interface for defining ways to override PG settings
-// outside of the usual manners (through the connection string/URL or env vars).
-// An example would be having specific flags that can be used to set database
-// connect parameters.
-type Overrideable interface {
-	Override() string
-}
-
-// OverrideDBName is a type for overriding the database name used to connect.
-// To use it, one casts a string of the database name as an OverrideDBName
-type OverrideDBName string
-
-func (o OverrideDBName) Override() string {
-	return string(o)
-}
-
-// ErrInvalidSSLMode is the error when the provided SSL mode is not one of the
-// values that PostgreSQL supports.
-type ErrInvalidSSLMode struct {
-	given string
-}
-
-func (e *ErrInvalidSSLMode) Error() string {
-	return "invalid SSL mode: " + e.given
-}
-
-const (
-	// envSSLMode is the environment variable key for SSL mode.
-	envSSLMode = "PGSSLMODE"
-)
-
-var sslmodeRegex = regexp.MustCompile("sslmode=([a-zA-Z-]+)")
-
-// determineTLS attempts to match SSL mode to a known PostgreSQL supported value.
-func determineTLS(connStr string) (string, error) {
-	res := sslmodeRegex.FindStringSubmatch(connStr)
-	var sslmode string
-	if len(res) == 2 {
-		sslmode = res[1]
-	} else {
-		sslmode = os.Getenv(envSSLMode)
-	}
-
-	if sslmode == "" {
-		return "", nil
-	}
-
-	switch sslmode {
-	case "require", "disable", "allow", "prefer", "verify-ca", "verify-full":
-		return sslmode, nil
-	default:
-		return "", &ErrInvalidSSLMode{given: sslmode}
-	}
-}
 
 // Connect returns a SQLX database corresponding to the provided connection
 // string/URL, env variables, and any provided overrides.
